f0eb05b33fa43869bf9cce43995cd8fcfa955b9c
Esquire <ackon.k.richard@gmail.com>
2017-06-25 01:30:43 +0000

    first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\theresa\desktop\progra~2\django~1\selfma~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ src/smghApi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SmghapiConfig(AppConfig):
+    name = 'smghApi'
