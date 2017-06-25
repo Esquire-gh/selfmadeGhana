96ec7b75037efc8ee4b547b574732313e099a5dc
Esquire <ackon.k.richard@gmail.com>
2017-06-25 08:07:18 +0000

    mysql setup complete

--- src/smgh/settings.py
+++ src/smgh/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'smgh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'smgh_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
