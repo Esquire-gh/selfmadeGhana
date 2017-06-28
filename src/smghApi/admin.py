31b1b4e710a32aa21075a0426c569ac52e314f75
Esquire <ackon.k.richard@gmail.com>
2017-06-28 22:47:14 +0000

    created db models

--- src/smghApi/admin.py
+++ src/smghApi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article, Comment, Discussion, Suggestion
 
-# Register your models here.
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Discussion)
+admin.site.register(Suggestion)
--- src/smghApi/models.py
+++ src/smghApi/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+tags = (
+	('Entrepreneurship', 'Entrepreneurship'),
+	('Politics', 'Politics'),
+	('Personal development', 'Personal development'),
+	('Startups','Startups'),
+	('Religion', 'Religion')
+	)
+
+class Article(models.Model):
+	title = models.CharField(max_length = 300)
+	content = models.TextField()
+	category = models.CharField(choices=tags, max_length=40)
+	data_created = models.DateField(auto_now_add = True)
+	image = models.FileField(upload_to='media/')
+	views = models.PositiveIntegerField(blank=True, default=0)
+	shares = models.PositiveIntegerField(blank=True, default=0)
+
+	def __str__(self):
+		return self.title
+
+
+class Comment(models.Model):
+	message = models.TextField()
+	sender = models.CharField(max_length=100, blank = True, null = True)
+	article = models.ForeignKey(Article, null=False)
+
+
+
+class Discussion(models.Model):
+	topic = models.TextField()
+	category = models.CharField(choices=tags, max_length=40)
+
+	def __str__(self):
+		return self.topic
+
+class Suggestion(models.Model):
+	message = models.TextField()
+	sender = models.CharField(max_length=100, blank = True, null = True)
+	discussion = models.ForeignKey(Discussion, null=False)
