68bf3a27dfbf4f94182348d929801d845f020d89
Esquire <ackon.k.richard@gmail.com>
2017-06-29 22:15:34 +0000

    author model

--- src/smghApi/models.py
+++ src/smghApi/models.py
@@ -8,8 +8,16 @@ tags = (
 	('Religion', 'Religion')
 	)
 
+class Author(models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return (self.first_name + " " + self.last_name)
+
 class Article(models.Model):
 	title = models.CharField(max_length = 300)
+	athor = models.ForeignKey(Author, blank = True, null=True)
 	content = models.TextField()
 	category = models.CharField(choices=tags, max_length=40)
 	data_created = models.DateField(auto_now_add = True)
